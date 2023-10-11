b17ffca347df85998f4b89397634093f6d2a61b6
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-10-11 13:17:10 +0200

    EG-11-splash integration

--- EGProject/App.js
+++ EGProject/App.js
@@ -5,15 +5,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './screens/Home';
 import  Header  from './components/Header';
-import SplashScreen from './components/SplashScreen';
+
+import { useEffect } from "react";
+import BootSplash from "react-native-bootsplash";
 
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <SplashScreen/>
-    </View>
-  );
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
+
+  return <Text>My awesome app</Text>;
 }
 export default App
 
