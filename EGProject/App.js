485d7ded9ac175547856a587f729322e87ff684f
esetedos <esther.fernandez@ikasle.aeg.eus>
2023-10-16 09:19:01 +0200

    [MERGE] con splashTest

--- EGProject/App.js
+++ EGProject/App.js
@@ -9,12 +9,27 @@ import SplashScreen from './components/SplashScreen';
 import MyStack from './components/MyStack.js';
 
 
+
+import { useEffect } from "react";
+import BootSplash from "react-native-bootsplash";
+
+
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <MyStack />
     </NavigationContainer>
-    
   );
 }
 export default App
