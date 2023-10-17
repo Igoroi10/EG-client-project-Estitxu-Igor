bcf335c3a9624a237ca919e759c1468d9180670e
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-10-17 13:31:58 +0200

    EG-23 [NEW] asyncStorage implementation and test

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar, ScrollView, StyleSheet, Text, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './screens/Home';
@@ -11,9 +11,10 @@ import Profile from './screens/Profile';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import { useEffect } from "react";
 import BootSplash from "react-native-bootsplash";
+
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -23,7 +24,14 @@ const App = () => {
   useEffect(() => {
     const init = async () => {
       // …do multiple sync or async tasks
-    };
+      console.log("Enters init function")
+    const value = "User data";
+
+    storeData(value);
+    const user = getData()
+    console.log(user);
+    
+  };
 
     init().finally(async () => {
       await BootSplash.hide({ fade: true });
@@ -53,3 +61,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('my-key', jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+  console.log("set passed")
+  
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('my-key');
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
