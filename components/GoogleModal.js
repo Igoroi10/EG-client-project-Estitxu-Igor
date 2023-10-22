8ce41724527ed5cfb2f3a3e588e0af3586bc5043
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-10-22 22:02:28 +0200

    EG-9 spinner block

--- components/GoogleModal.js
+++ components/GoogleModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Modal from 'react-modal';
 import { View, Text, Image } from 'react-native';
 import styled from "styled-components/native";
@@ -9,11 +9,27 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 Modal.setAppElement('#yourAppElement');
 
 function GoogleSignIn() {
+  const [loadingGoogle, setGoogleLoad] = useEffect(false)
+
+  const googlePressButton = () => {
+    setGoogleLoad = true
+    onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+  
+  }
+
     return (
-      <GoogleButton
-        title="Google Sign-In"
-        onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-      />
+    <>
+      { !loadingGoogle &&
+        <GoogleButton title="Google Sign-In" onPress={googlePressButton()}/>
+      }
+      {loadingGoogle && 
+        <GoogleButton class="fa fa-spinner"/>
+      }
+    </>
+      
+
+    
+
     );
   }
   
@@ -40,6 +56,7 @@ const GoogleModal = ({logStatus}) =>{
                 </ModalTemplate>
             )
 }
+
 
 
 
