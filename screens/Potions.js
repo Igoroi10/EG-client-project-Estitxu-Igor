07249b058a7a5657d2a90c12e88415ebb14e9710
Unai <unai.heredero@ikasle.aeg.eus>
2023-10-24 13:42:09 +0200

    EG-40 [TODO] Selecionando 2 ingredientes se llama a potionHandler con estos, pero se puede repetir. Hay que añadir boton de crear pocion

--- screens/Potions.js
+++ screens/Potions.js
@@ -1,37 +1,55 @@
-import React from 'react'
-import { FlatList, Text, Button } from 'react-native'
-import styled from 'styled-components/native'
-import fakeIngredients from '../fakeData/fakeIngredients.json'
-import potionHandler from '../helpers/potionHandler'
+import React from 'react';
+import { FlatList, Text, Button, PushNotificationIOS } from 'react-native';
+import styled from 'styled-components/native';
+import fakeIngredients from '../fakeData/fakeIngredients.json';
+import potionHandler from '../helpers/potionHandler';
 
-const Potions = () =>{
-    return(
-        <>
-            <HeadContainer>
-                <Text>Potion creation</Text>
-            </HeadContainer>
-            <ContentContainer>
+const Potions = () => {
 
-                 <FlatList 
-                        data={fakeIngredients}
-                        renderItem={({item}) => (
-                            <Button title = {item.name}/>
-                        )}
-                        keyExtractor={item => item.key}
-                />
-            </ContentContainer>
-        </>
-    )
-} 
+let ingredientArray= [];
+
+  const handleIngredientPress = (item) => {
+    const selectedIngredient = item;
+    ingredientArray.push(selectedIngredient)
+    //alert(`Ingredient: ${ingredientArray}.`);
+    
+    //alert(ingredientArray.length)
+
+    if(ingredientArray.length == 2){
+        potionHandler(ingredientArray[0], ingredientArray[1])
+    }
+  };
+
+  return (
+    <>
+      <HeadContainer>
+        <Text>Potion creation</Text>
+      </HeadContainer>
+      <ContentContainer>
+        <FlatList
+          data={fakeIngredients}
+          renderItem={({ item }) => (
+            <Button
+              title={item.name}
+              onPress={() => handleIngredientPress(item)}
+            />
+          )}
+          keyExtractor={(item) => item.key}
+        />
+      </ContentContainer>
+    </>
+  );
+};
 
 const HeadContainer = styled.View`
-    margin-top: 5%;
-    height: 15%
-`
+  margin-top: 5%;
+  height: 15%;
+`;
 
 const ContentContainer = styled.View`
-    flex-direction: column;
-    justify-content: space-between;
-    height: 40%;
-`
-export default Potions
+  flex-direction: column;
+  justify-content: space-between;
+  height: 40%;
+`;
+
+export default Potions;
