018060d8d7b20032dc56ba2904ea5913650a2d86
esetedos <esther.fernandez@ikasle.aeg.eus>
2023-10-30 10:07:32 +0100

    EG-39 [MERGE] with develop

--- screens/Potions.js
+++ screens/Potions.js
@@ -1,53 +1,159 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, Text, Button, View, Image } from 'react-native';
+import { FlatList, Text, View, TouchableOpacity , Image} from 'react-native';
 import styled from 'styled-components/native';
+import fakeIngredients from '../fakeData/fakeIngredients.json';
+import potionHandler from '../helpers/potionHandler';
 import axios from 'axios';
 
+
 const Potions = () => {
-    const [ingredientsData, setIngredientsData] = useState([]);
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [ingredientsData, setIngredientsData] = useState([]);
 
-    useEffect(() => {
-        async function fetchIngredients() {
-            try {
-                const response = await axios.get('http://192.168.0.26:3000/api/ingredients/');
-                const responseData = response.data.data;
-                setIngredientsData(responseData);
-            } catch (error) {
-                console.error('Error al obtener los ingredientes:', error);
-            }
+
+  useEffect(() => {
+    async function fetchIngredients() {
+        try {
+            const response = await axios.get('http://192.168.1.165:3000/api/ingredients/');
+            const responseData = response.data.data;
+            setIngredientsData(responseData);
+        } catch (error) {
+            console.error('Error al obtener los ingredientes:', error);
         }
+    }
 
-        fetchIngredients();
-    }, []);
+    fetchIngredients();
+}, []);
 
-    const renderIngredientItem = ({ item }) => {
-        return (
-            <View style={styles.itemContainer}>
-                <Button title={item.name} />
+  const handleIngredientPress = (item) => {
+    if (selectedIngredients.length < 2 && !selectedIngredients.includes(item)) {
+      setSelectedIngredients([...selectedIngredients, item]);
+    } else {
+      alert("Cannot use the same ingredient twice or select more than 2 ingredients");
+    }
+  };
+
+  const createPotion = () => {
+    if (selectedIngredients.length === 2) {
+      potionHandler(selectedIngredients[0], selectedIngredients[1]);
+    } else {
+      alert("Select 2 ingredients to create a potion");
+    }
+  };
+
+  const deleteIngredients = () => {
+    setSelectedIngredients([]);
+  };
+
+  return (
+    <>
+      <HeadContainer>
+        <Text>Potion creation</Text>
+      </HeadContainer>
+      <ContentContainer>
+        {selectedIngredients.length < 2 && (
+          <FlatList
+            data={ingredientsData}
+            renderItem={({ item }) => (
+              <IngredientButton
+                onPress={() => handleIngredientPress(item)}
+                style={
+                  selectedIngredients.includes(item)
+                    ? { backgroundColor: 'gray' }
+                    : {}
+                }
+              >
                 <Image source={{ uri: item.image }} style={styles.image} />
-            </View>
-        );
-    };
-
-    return (
-        <>
-            <HeadContainer>
-                <Text>Potion creation</Text>
-            </HeadContainer>
-            <ContentContainer>
-                {ingredientsData.length > 0 ? (
-                    <FlatList
-                        data={ingredientsData}
-                        renderItem={renderIngredientItem}
-                        keyExtractor={item => item.key.toString()}
-                    />
-                ) : (
-                    <Text>Loading...</Text>
-                )}
-            </ContentContainer>
-        </>
-    );
+                <ButtonText>{item.name}</ButtonText>
+              </IngredientButton>
+            )}
+            keyExtractor={(item) => item.key}
+          />
+        )}
+        {selectedIngredients.length === 2 && (
+          <SelectedIngredientsContainer>
+            <SelectedIngredientsText>
+              Selected Ingredients: {selectedIngredients[0].name} and {selectedIngredients[1].name}
+            </SelectedIngredientsText>
+          </SelectedIngredientsContainer>
+        )}
+        {selectedIngredients.length === 2 && (
+          <ButtonContainer>
+            <CreateButton onPress={createPotion}>
+              <ButtonText>Create Potion</ButtonText>
+            </CreateButton>
+          </ButtonContainer>
+        )}
+        <ButtonContainer>
+        <DeleteIngredientsButton onPress={deleteIngredients}>
+          <ButtonText>Delete Ingredients</ButtonText>
+        </DeleteIngredientsButton>
+          </ButtonContainer>
+      </ContentContainer>
+    </>
+  );
 };
+
+const HeadContainer = styled.View`
+  margin-top: 5%;
+  height: 15%;
+`;
+
+const ContentContainer = styled.View`
+  flex-direction: column;
+  justify-content: space-between;
+  height: 60%;
+`;
+
+const ButtonContainer = styled.View`
+  align-items: center;
+  width: 100%;
+`;
+
+const IngredientButton = styled.TouchableOpacity`
+  background-color: #007BFF;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin-top: 10px;
+`;
+
+const CreateButton = styled.TouchableOpacity`
+  background-color: green;
+  border-radius: 50px;
+  height: 50px;
+  width: 150px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+const DeleteIngredientsButton = styled.TouchableOpacity`
+  background-color: #ff0000;
+  border-radius: 50px;
+  height: 50px;
+  width: 150px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+`;
+
+const SelectedIngredientsText = styled.Text`
+  color: #000;
+  font-size: 20px;
+  text-align: center;
+`;
+
+const SelectedIngredientsContainer = styled.View`
+  align-items: center;
+  text-align: center;
+  width: 100%;
+`;
 
 const styles = {
     itemContainer: {
@@ -60,16 +166,5 @@ const styles = {
         height: 50,
     },
 };
-
-const HeadContainer = styled.View`
-    margin-top: 5%;
-    height: 15%;
-`;
-
-const ContentContainer = styled.View`
-    flex-direction: column;
-    justify-content: space-between;
-    height: 40%;
-`;
 
 export default Potions;
