4b8219e2ae568a49b450a51222162af3145f7253
Unai <unai.heredero@ikasle.aeg.eus>
2023-11-07 20:34:12 +0100

    [UPDATE] En la pantalla Tower, se oculta el botón al hacer click y towerAccess es true

--- screens/Tower.js
+++ screens/Tower.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import { TouchableOpacity } from 'react-native';
-import { getData } from "../helpers/localStorage";
-import axios from "axios";
-import Toast from 'react-native-toast-message'; // Import Toast from react-native-toast-message
+import { getData } from '../helpers/localStorage';
+import axios from 'axios';
+import Toast from 'react-native-toast-message';
 
 const View = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Text = styled.Text`
-    font-size: 16px;
-    font-weight: bold;
-    color: white;
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
 `;
 
 const ImageBackground = styled.ImageBackground`
-    flex: 1;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ButtonText = styled.Text`
-    color: white;
-    font-size: 16px;
-    font-weight: bold;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
 `;
 
 const Button = styled.TouchableOpacity`
@@ -38,52 +38,52 @@ const Button = styled.TouchableOpacity`
 `;
 
 const Tower = () => {
+  const [towerState, setTowerState] = useState(null);
+  const [potionState, setPotion] = useState(null);
+  const [isButtonVisible, setButtonVisible] = useState(true);
 
-    const [towerState, setTowerState] = useState(null);
-    const [potionState, setPotion] = useState(null);
+  const checkTowerAccess = async () => {
+    const data = await getData();
+    const user = data[0];
 
-    const checkTowerAccess = async () => {
-        const data = await getData();
-        const user = data[0];
+    const response = await axios.get('https://fly-eg-staging.fly.dev/api/users/');
+    const responseData = response.data.data;
+    const currentUserData = responseData.filter((element) => element.email === user.email);
+    const currentUser = currentUserData[0];
 
-        const response = await axios.get('https://fly-eg-staging.fly.dev/api/users/');
-        const responseData = response.data.data;
-        const currentUserData = responseData.filter((element) => element.email === user.email);
-        const currentUser = currentUserData[0];
-
-        if (currentUser.towerAccess) {
-            const accesText = "ACCESO GARANTIZADO, BIENVENIDO " + currentUser.rol + " " + currentUser.name;
-            setTowerState('corruptScroll')
-            Toast.show({
-                type: 'success', // Toast type
-                position: 'bottom', // Toast position
-                text1: 'Acceso al Torreón', // Title
-                text2: accesText, // Message
-            });
-            
-            // ACCEDER A PANTALLA PERGAMINO
-        } else {
-            const accesText = "ACCESO DENEGADO, FUERA DE AQUÍ!";
-            Toast.show({
-                type: 'error', // Toast type
-                position: 'bottom', // Toast position
-                text1: 'Acceso al Torreón', // Title
-                text2: accesText, // Message
-            });
-        }
+    if (currentUser.towerAccess) {
+      const accesText = 'ACCESO GARANTIZADO, BIENVENIDO ' + currentUser.rol + ' ' + currentUser.name;
+      setTowerState('corruptScroll');
+      Toast.show({
+        type: 'success', // Toast type
+        position: 'bottom', // Toast position
+        text1: 'Acceso al Torreón', // Title
+        text2: accesText, // Message
+      });
+      setButtonVisible(false);
+      
+    } else {
+      const accesText = 'ACCESO DENEGADO, FUERA DE AQUÍ!';
+      Toast.show({
+        type: 'error', // Toast type
+        position: 'bottom', // Toast position
+        text1: 'Acceso al Torreón', // Title
+        text2: accesText, // Message
+      });
     }
+  };
 
-    return (
-        <ImageBackground
-            source={require('../assets/tower.png')}
-        >
-            <View>
-                <Button onPress={checkTowerAccess}>
-                    <ButtonText>ACCEDER AL TORREÓN</ButtonText>
-                </Button>
-            </View>
-        </ImageBackground>
-    )
-}
+  return (
+    <ImageBackground source={require('../assets/tower.png')}>
+      <View>
+        {isButtonVisible && (
+          <Button onPress={checkTowerAccess}>
+            <ButtonText>ACCEDER AL TORREÓN</ButtonText>
+          </Button>
+        )}
+      </View>
+    </ImageBackground>
+  );
+};
 
 export default Tower;
