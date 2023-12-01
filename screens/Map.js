fa7347cf9826dd27eb0a0e2f5acad6933e4a5522
esetedos <esther.fernandez@ikasle.aeg.eus>
2023-12-01 10:46:41 +0100

    EG-164 [NEW] map para mostrar users

--- screens/Map.js
+++ screens/Map.js
@@ -332,6 +332,25 @@ const styles = StyleSheet.create({
                 ) : null
                 )}
 
+                {globalState.userList.map((user) => user ? (
+                  // user.found == false ? (
+
+                    <Marker
+                      key={user._id}
+                      coordinate={{
+                        latitude: user.latitude,
+                        longitude: user.longitude,
+                      }}
+                      title={user.name}
+                    >
+                      <Image
+                        source={{ uri: user.imgURL }} 
+                        style={{ width: 40, height: 40 }} />
+                    </Marker>
+                  // ) : null
+                ) : null
+                )}
+
               </MapView>
             </MapContainer>
           )}
