7d858c1a27d61398f065b71cd932b2d6daaa61eb
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-17 11:17:53 +0100

    EG-192 [UPDATE] socket emit again available and opened tomb image change

--- helpers/cryptFunction.js
+++ helpers/cryptFunction.js
@@ -3,7 +3,7 @@ import socket from "./socket";
 const cryptButton = (num,email) => {
     console.log("emit of crypt")
     const data = {num: num, email: email}
-    // socket.emit("objectRetrieval", data)
+    socket.emit("objectRetrieval", data)
 }
 
 export default cryptButton
