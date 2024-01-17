e8ee1f8902e4ed607bebfc6a355c24da4a702f9f
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2024-01-17 10:51:35 +0100

    EG-193 [UPDATE] now images triggers socket when tapped

--- components/CryptModal.js
+++ components/CryptModal.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect, useContext} from "react";
 import Modal from 'react-modal';
-import { View, Text, Image, ActivityIndicator, StyleSheet, FlatListComponent } from 'react-native';
+import { View, Text, Image, ActivityIndicator, StyleSheet, TouchableHighlight } from 'react-native';
 import styled from "styled-components/native";
 
 import { Context } from '../AppContext';
+
+import cryptButton from "../helpers/cryptFunction";
 
 
 Modal.setAppElement('#yourAppElement');
@@ -13,13 +15,29 @@ const CryptModal = () =>{
 
   return(
     <ModalPage visible = {globalState.insideCrypt?true:false}>
-          <ModalImage0 source={globalState.crypt[0]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')} />
-          {/* Botón invisible */}
-          <ModalImage1 source={globalState.crypt[1]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')} />
-          {/* Botón invisible */}
-          <ModalImage2 source={globalState.crypt[2]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')} />
-          {/* Botón invisible */}
-          <ModalImage3 source={globalState.crypt[3]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')} />
+      <Slot0 onPress={() => cryptButton(0, globalState.user.email)} >
+        <View>
+          <ModalImage source={globalState.crypt[0]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')} />
+        </View>
+      </Slot0>
+      <Slot1 onPress={() => cryptButton(1, globalState.user.email)} >
+        <View>
+          <ModalImage source={globalState.crypt[1]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')}/> 
+        </View>
+      </Slot1>
+      <Slot2 onPress={() => cryptButton(2, globalState.user.email)} >
+        <View>
+          <ModalImage source={globalState.crypt[2]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')}/>
+        </View>
+      </Slot2>
+      <Slot3 onPress={() => cryptButton(3, globalState.user.email)}>
+        <View>
+          <ModalImage source={globalState.crypt[3]==="closed"?require('../assets/tower.png'):require('../assets/villano.png')}/>
+        </View>
+      </Slot3>
+        
+        
+       
     </ModalPage>
 
   )
@@ -31,7 +49,27 @@ const ModalPage = styled.View`
   width: 100%;
   height: 100%;
 `
-
+const Slot0 = styled.TouchableHighlight`
+  width: 50%;
+  height: 50%;
+`
+const Slot1 = styled.TouchableHighlight`
+  position: absolute;
+  width: 50%;
+  height: 50%;
+  margin-left: 50%;
+`
+const Slot2 = styled.TouchableHighlight`
+  width: 50%;
+  height: 50%;
+`
+const Slot3 = styled.TouchableHighlight`
+  position: absolute;
+  width: 50%;
+  height: 51%;
+  margin-left: 50%;
+  margin-top: 94.5%;
+`
 
 const ModalTemplate = styled.Modal`
   position: absolute;
@@ -39,30 +77,9 @@ const ModalTemplate = styled.Modal`
   height: 100%;
 `;
 
-const ModalImage0 = styled.Image`
-  width: 50%;
-  height: 50%;
-  
-`;
-
-const ModalImage1 = styled.Image`
-  position: absolute;
-  width: 50%;
-  height: 50%;
-  margin-left: 50%;
-`;
-
-const ModalImage2 = styled.Image`
-  width: 50%;
-  height: 50%;
-`;
-
-const ModalImage3 = styled.Image`
-  position: absolute;
-  width: 50%;
-  height: 51%;
-  margin-left: 50%;
-  margin-top: 94.5%;
+const ModalImage = styled.Image`
+  width: 100%;
+  height: 100%;
 `;
 
 
---
+++ helpers/cryptFunction.js
@@ -0,0 +1,9 @@
+import socket from "./socket";
+
+const cryptButton = (num,email) => {
+    console.log("emit of crypt")
+    const data = {num: num, email: email}
+    // socket.emit("objectRetrieval", data)
+}
+
+export default cryptButton
