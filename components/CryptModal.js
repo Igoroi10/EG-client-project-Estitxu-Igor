22e5900627d485131ad84ab7dc6e6d2cf18a876b
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-01-18 17:12:09 +0100

    [FIX] desactivar botones una vez abiertas las criptas

--- components/CryptModal.js
+++ components/CryptModal.js
@@ -17,20 +17,20 @@ const CryptModal = () =>{
   return(
     <ModalPage visible = {globalState.insideCrypt?true:false}>
       <TopView>
-        <Slot0 onPress={() => cryptButton(0, globalState.user.email)} >
+        <Slot0 onPress={() => (globalState.items[0] && globalState.items[0].retrieved?null:cryptButton(0, globalState.user.email))} >
           <ModalImage source={globalState.items[0] && globalState.items[0].retrieved===true?require('../assets/tomb_open.png'):require('../assets/tomb_closed.png')} />
         </Slot0>
-        <Slot1 onPress={() => cryptButton(1, globalState.user.email)} >
+        <Slot1 onPress={() => (globalState.items[1] && globalState.items[1].retrieved?null:cryptButton(1, globalState.user.email))} >
               <ModalImage source={globalState.items[1] && globalState.items[1].retrieved===true?require('../assets/tomb_open.png'):require('../assets/tomb_closed.png')}/> 
         </Slot1>
       </TopView>
       <TopView>
-          <Slot0 onPress={() => cryptButton(2, globalState.user.email)} >
+          <Slot0 onPress={() =>(globalState.items[2] && globalState.items[2].retrieved?null: cryptButton(2, globalState.user.email))} >
 
               <ModalImage source={globalState.items[2] && globalState.items[2].retrieved===true?require('../assets/tomb_open.png'):require('../assets/tomb_closed.png')}/>
 
           </Slot0>
-          <Slot0 onPress={() => cryptButton(3, globalState.user.email)}>
+          <Slot0 onPress={() => (globalState.items[3] && globalState.items[3].retrieved?null:cryptButton(3, globalState.user.email))}>
               <ModalImage source={globalState.items[3] && globalState.items[3].retrieved===true?require('../assets/tomb_open.png'):require('../assets/tomb_closed.png')}/>
           </Slot0>
         </TopView>
