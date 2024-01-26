823de12f5a6235e45c5b356607380a51655f590f
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-01-26 09:26:27 +0100

    EG-207 [UPDATE] pantalla pociones

--- helpers/potionHandler.js
+++ helpers/potionHandler.js
@@ -4,70 +4,46 @@ import socket from '../helpers/socket';
 
 
 export default async function potionHandler(ingredient1, ingredient2, user){
-    console.log("****************************AFFECTIONS*************************")
-    const affections = await fetchAffections();
-    console.log(affections);
-    // const commonEffects = [];
-    // ingredient1.effects.forEach(el=> {
-    //     commonEffects.push(el);
-    // });
-
-    // ingredient2.effects.forEach(el2=> {
-    //     let check = 0;
-    //     ingredient1.effects.forEach(el1 => {
-    //         if(el2 === el1)
-    //             check++;
-    //     })
-
-    //     if(check === 0)
-    //         commonEffects.push(el2)
-    // })
-
-    // let ingredientName = "Potion of ";
-
-    // commonEffects.forEach(el => {
-    //     ingredientName += el+", ";
-    // })
-
-    // ingredientName = ingredientName.substring(0, ingredientName.length-2)
-
-   // alert(ingredientName)
+    const affections = await fetchAffections(user);
+    // console.log(affections);
 
 
     let potion = [];
 
-    ingredient1.effects.forEach(ingEffect => {
-        affections.forEach(affection => {
-            affection.healing_effects.forEach(affectionEffect => {
-                if(affectionEffect === ingEffect){
-                    potion = affection;
-                }
+    if(ingredient1.effects){
+        ingredient1.effects.forEach((ingEffect) => {
+            affections.forEach((affection) => {
+                affection.healing_effects.forEach(affectionEffect => {
+                    if(affectionEffect === ingEffect){
+                        potion = affection;
+                    }
+                })
             })
+        
         })
-    })
-
-    ingredient2.effects.forEach(ingEffect => {
-        potion.healing_effects.forEach(affectionEffect => {
-            if(ingEffect === affectionEffect){
-                //crear pocion
-                if(user){
-                    cureCurse(user, potion.id);
-                }
-
-                return potion.name
-            }
-        })
-    })
-
+        if(potion){
+            ingredient2.effects.forEach(ingEffect => {
+                potion.healing_effects.forEach(affectionEffect => {
+                    if(ingEffect === affectionEffect){
+                        //crear pocion
+                        if(user){
+                            cureCurse(user.email, potion.id);
+                            return potion.name
+        
+                        }
+                    }
+                })
+            })
     
+        }
+    }
     
-
 
     return "failedPotion";
 }
 
 
-async function fetchAffections() {
+async function fetchAffections(user) {
     try {
       const response = await axios.get('https://fly-eg-staging.fly.dev/api/affection/');
       const responseData = response.data.data;
